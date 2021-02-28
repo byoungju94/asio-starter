e176585e2804a41bea5f378e0e534ac1194898b1
byoungju94 <byoungju94@outlook.com>
2021-03-01 02:00:08 +0900

    add mutex for wating response from server

--- asio-starter/Main.cpp
+++ asio-starter/Main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 
 #ifdef _WIN32
@@ -13,6 +14,46 @@ int main()
 	asio::error_code ec;
 
 	asio::io_context context;
+
+	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("127.0.0.1", ec), 80);
+
+	asio::ip::tcp::socket socket(context);
+
+	socket.connect(endpoint, ec);
+
+	if (!ec)
+	{
+		std::cout << "Connected!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Failed to connect to address:\n" << ec.message() << std::endl;
+	}
+
+	if (socket.is_open())
+	{
+		std::string sRequest =
+			"GET /index.html HTTP/1.1\r\n"
+			"Host: example.com\r\n"
+			"Connection: close\r\n\r\n";
+
+		socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
+
+		using namespace std::chrono_literals;
+		std::this_thread::sleep_for(200ms);
+
+		size_t bytes = socket.available();
+		std::cout << "Bytes Available: " << bytes << std::endl;
+
+		if (bytes > 0) 
+		{
+			std::vector<char> vBuffer(bytes);
+			socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), ec);
+
+			for (auto c : vBuffer)
+				std::cout << c;
+		}
+	}
 
 	return 0;
 }
